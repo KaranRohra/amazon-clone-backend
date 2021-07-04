bd45aa4b49107816dced78e5e3a2bde6fbe358b7
KaranRohra <karanrohra@KaranRohras-MacBook-Air.local>
2021-07-04 09:03:02 +0530

    [closes #35]: removed if condition

--- backend/settings.py
+++ backend/settings.py
@@ -91,9 +91,8 @@ DATABASES = {
     }
 }
 
-if(not os.environ.get("DEBUG")):
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
