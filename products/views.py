e99f47514b8b7d760e9129c58e311e32c8b1a0fd
KaranRohra <rohrakaran38@gmail.com>
2021-07-07 16:55:41 +0530

    [REF #37]: Added product-count api

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products import _private
 from products import models
@@ -19,3 +21,9 @@ class ProductsPageNumberApi(generics.ListAPIView):
 class ProductApi(viewsets.ModelViewSet):
     serializer_class = serializers.ProductSerializer
     queryset = models.Product.objects.all()
+
+
+@api_view(("GET",))
+def get_products_count(request):
+    number_of_products = _private.get_products_count_based_on_search_value(request.GET.get("search", ""))
+    return Response(number_of_products)
